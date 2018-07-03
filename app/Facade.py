07c0187cc932e4bcb37e5819bf6726eb485b8e0a
Heitor <heitor.augusto@ufrpe.br>
2018-07-03 19:23:42 -0300

    Corrigindo atributos privados

--- app/Facade.py
+++ app/Facade.py
@@ -8,74 +8,74 @@ from app.controllers import ControllerUsuario, ControllerCliente, ControllerRefo
 class Facade():
 
     def __init__(self):
-        __self.usuario = ControllerUsuario.ControllerUsuario()
-        __self.reforma = ControllerReforma.ControllerReforma()
-        __self.cliente = ControllerCliente.ControllerCliente()
-        __self.profissional = ControllerProfissional.ControllerProfissional()
-        __self.login = ControllerLogin.ControllerLogin()
+        self.__usuario = ControllerUsuario.ControllerUsuario()
+        self.__reforma = ControllerReforma.ControllerReforma()
+        self.__cliente = ControllerCliente.ControllerCliente()
+        self.__profissional = ControllerProfissional.ControllerProfissional()
+        self.__login = ControllerLogin.ControllerLogin()
     
     def inserirCliente(self, cpf, nome, telefone, senha):
-        return self.cliente.inserirCliente(cpf, nome, telefone, senha)
+        return self.__cliente.inserirCliente(cpf, nome, telefone, senha)
 
     def removerCliente(self,id):
-        return self.cliente.removerCliente(id)
+        return self.__cliente.removerCliente(id)
 
     def retornarCliente(self,id):
-        return self.cliente.retornarCliente(id)
+        return self.__cliente.retornarCliente(id)
 
     def retornarTodosClientes(self):
-        return self.cliente.retornarTodosClientes()
+        return self.__cliente.retornarTodosClientes()
 
     def atualizarCliente(self, id, cpf, nome, telefone, senha):
-        return self.cliente.atualizarCliente(id, cpf, nome, telefone, senha)
+        return self.__cliente.atualizarCliente(id, cpf, nome, telefone, senha)
     
     def login(self, telefone, senha):
-        return self.login.login(telefone, senha)
+        return self.__login.login(telefone, senha)
     
     def inserirProfissional(self,cpf,nome,telefone,senha,habilidades):
-        return self.profissional.inserirProfissional(cpf, nome, telefone, senha, habilidades)
+        return self.__profissional.inserirProfissional(cpf, nome, telefone, senha, habilidades)
 
     def removerProfissional(self,id):
-        return self.profissional.removerProfissional(id)
+        return self.__profissional.removerProfissional(id)
 
     def retornarProfissional(self,id):
-        return self.profissional.retornarProfissional(id)
+        return self.__profissional.retornarProfissional(id)
 
     def retornarTodosProfissionais(self):
-        return self.profissional.retornarTodosProfissionais()
+        return self.__profissional.retornarTodosProfissionais()
 
     def atualizarProfissional(self,id,cpf,nome,telefone,senha,habilidades):
-        return self.profissional.atualizarProfissional( id, cpf, nome, telefone, senha, habilidades)
+        return self.__profissional.atualizarProfissional( id, cpf, nome, telefone, senha, habilidades)
 
     def inserirReforma(self,id_cliente, datainicio, nome, descricao):
-        return self.reforma.inserirReforma(id_cliente, datainicio, nome, descricao)
+        return self.__reforma.inserirReforma(id_cliente, datainicio, nome, descricao)
 
     def removerReforma(self,id):
-        return self.reforma.removerReforma(id)
+        return self.__reforma.removerReforma(id)
 
     def retornarReforma(self,id):
-        return self.reforma.retornarReforma(id)
+        return self.__reforma.retornarReforma(id)
 
     def retornarTodasReformas(self):
-        return self.reforma.retornarTodasReformas()
+        return self.__reforma.retornarTodasReformas()
                 
     def atualizarReforma(self,id,id_cliente,datainicio,nome,descricao):#, id_status,id_profissional,preco) 
-        return self.reforma.atualizarReforma(id, id_cliente, datainicio, nome, descricao)  
+        return self.__reforma.atualizarReforma(id, id_cliente, datainicio, nome, descricao)  
                 
     def inserirReformaProfissional(self,id_reforma, id_profissional):
-        return self.reforma.inserirReformaProfissional(id_reforma, id_profissional)
+        return self.__reforma.inserirReformaProfissional(id_reforma, id_profissional)
     
     def inserirUsuario(self,telefone,senha):
-        return self.usuario.inserirUsuario(telefone, senha)
+        return self.__usuario.inserirUsuario(telefone, senha)
 
     def removerUsuario(self,id):
-        return self.usuario.removerUsuario(id)
+        return self.__usuario.removerUsuario(id)
 
     def retornarTodosUsuarios(self):
-        return self.usuario.retornarTodosUsuarios()
+        return self.__usuario.retornarTodosUsuarios()
 
     def retornarUsuario(self,id):
-        return self.usuario.retornarUsuario(id)
+        return self.__usuario.retornarUsuario(id)
 
     def atualizarUsuario(self,id,telefone,senha):
-        return self.usuario.atualizarUsuario(id, telefone, senha)
+        return self.__usuario.atualizarUsuario(id, telefone, senha)
