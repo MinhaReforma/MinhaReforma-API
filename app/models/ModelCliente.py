3b85071dd56290d251bcc7e36031037f9b70eefd
nanotrigueiro <nanotrigueiro@hotmail.com>
2018-07-11 20:32:28 -0300

    Nullable false

--- app/models/ModelCliente.py
+++ app/models/ModelCliente.py
@@ -4,7 +4,7 @@ class Cliente(db.Model):
     __tablename__ = "cliente"
 
     id = db.Column(db.Integer, primary_key=True)
-    id_pessoa = db.Column(db.Integer, db.ForeignKey('pessoa.id'), unique=True)
+    id_pessoa = db.Column(db.Integer, db.ForeignKey('pessoa.id'), unique=True, nullable=False)
     reformas = db.relationship('Reforma', backref='cliente', lazy='joined')
     negociacoes = db.relationship('NegociacaoPreco', backref='cliente')
     conversas = db.relationship('Conversa', backref='cliente')
