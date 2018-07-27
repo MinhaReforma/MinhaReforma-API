113e5419b08afc5a7a7e70b245252251a480b52b
Heitor <heitor.augusto@ufrpe.br>
2018-07-27 02:17:09 -0300

    fix commit

--- app/controllers/ControllerAvaliacao.py
+++ app/controllers/ControllerAvaliacao.py
@@ -219,10 +219,10 @@ class ControllerAvaliacao():
         if g == []:
             return {'sucesso':False, 'mensagem':'não há avaliações.'}
         
-        h = Profissional.query.filter_by(id=g.id_avaliador).first()
 
         lista = list()
         for i in g:
+            h = Profissional.query.filter_by(id=i.id_avaliador).first()
             lista.append({'id_avaliador':i.id_avaliador, 'id_avaliado':i.id_avaliado, 'id_reforma':i.id_reforma, 'profissional':{'id':h.id,'cpf':h.pessoa.cpf,'nome':h.pessoa.nome,'telefone':h.pessoa.usuario.telefone} , 'mensagem':i.mensagem, 'nota':i.nota, 'tipo':i.tipo})
         
         return {'sucesso':True, 'mensagem':'todas as avaliações retornadas com sucesso.', 'avaliacoes':lista}
@@ -235,10 +235,10 @@ class ControllerAvaliacao():
         if g == []:
             return {'sucesso':False, 'mensagem':'não há avaliações.'}
         
-        h = Cliente.query.filter_by(id=g.id_avaliador).first()
         
         lista = list()
         for i in g:
+            h = Cliente.query.filter_by(id=i.id_avaliador).first()
             lista.append({'id_avaliador':i.id_avaliador, 'id_avaliado':i.id_avaliado, 'id_reforma':i.id_reforma, 'cliente':{'id':h.id,'cpf':h.pessoa.cpf,'nome':h.pessoa.nome,'telefone':h.pessoa.usuario.telefone},'mensagem':i.mensagem, 'nota':i.nota, 'tipo':i.tipo})
         
         return {'sucesso':True, 'mensagem':'todas as avaliações retornadas com sucesso.', 'avaliacoes':lista}
